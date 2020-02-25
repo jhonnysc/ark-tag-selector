968a49731241500933dc3e55b7a35f5405274a7e
jhonny <jhonny.sicol@gmail.com>
2020-02-25 15:40:36 -0300

    Updates

---
+++ precache-manifest.ce003679f1405866879fd1d71f837f5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "872af85ff398db031948a50e9ce636b9",
+    "url": "/ark-tag-selector/index.html"
+  },
+  {
+    "revision": "3677b549314c24aeca97",
+    "url": "/ark-tag-selector/static/js/2.d4139c5c.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/ark-tag-selector/static/js/2.d4139c5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b6bb85103ab5b5612ef",
+    "url": "/ark-tag-selector/static/js/main.20f289d9.chunk.js"
+  },
+  {
+    "revision": "add242813823ac8cb4d4",
+    "url": "/ark-tag-selector/static/js/runtime-main.ba538605.js"
+  },
+  {
+    "revision": "35c3f3d2c90eda1178bd95886a05f1f1",
+    "url": "/ark-tag-selector/static/media/jessica.35c3f3d2.jpg"
+  }
+]);
